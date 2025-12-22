9df5b430a20532fc98be1f6b40e0bb4910b77ccd
Ritabrata Ghosh <76sonali40@gmail.com>
2025-12-22 14:35:45 +0530

    trpc setup done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
-import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-function page() {
-  return <div className={cn("text-red-500 font-extrabold")}>page</div>;
-}
+import { getQueryClient, trpc } from "./trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import Client from "./client";
+
+const page = () => {
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+
+  return (
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Client />
+      </HydrationBoundary>
+    </div>
+  );
+};
 
 export default page;
