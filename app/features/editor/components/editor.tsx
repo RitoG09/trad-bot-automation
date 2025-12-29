40680c40e4dcc726a201eb63661e1ca58c563d71
Ritabrata Ghosh <76sonali40@gmail.com>
2025-12-29 23:57:08 +0530

    react-flow init

--- app/features/editor/components/editor.tsx
+++ app/features/editor/components/editor.tsx
@@ -1,7 +1,26 @@
 "use client";
 
+import { useState, useEffect, useCallback } from "react";
+import {
+  ReactFlow,
+  applyNodeChanges,
+  applyEdgeChanges,
+  addEdge,
+  Node,
+  Edge,
+  NodeChange,
+  EdgeChange,
+  Connection,
+} from "@xyflow/react";
 import { ErrorView, LoadingView } from "@/components/entity-components";
 import { useSuspenseSingleWorkflow } from "../../workflows/hooks/use-workflows";
+import "@xyflow/react/dist/style.css";
+
+const initialNodes = [
+  { id: "n1", position: { x: 0, y: 0 }, data: { label: "Node 1" } },
+  { id: "n2", position: { x: 0, y: 100 }, data: { label: "Node 2" } },
+];
+const initialEdges = [{ id: "n1-n2", source: "n1", target: "n2" }];
 
 export const EditorLoading = () => {
   return <LoadingView msg="Loading editor..." />;
@@ -13,5 +32,36 @@ export const EditoError = () => {
 
 export const Editor = ({ workflowId }: { workflowId: string }) => {
   const { data: workflow } = useSuspenseSingleWorkflow(workflowId);
-  return <p>{JSON.stringify(workflow, null, 2)}</p>;
+
+  const [nodes, setNodes] = useState<Node[]>(initialNodes);
+  const [edges, setEdges] = useState<Edge[]>(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes: NodeChange[]) =>
+      setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+    []
+  );
+  const onEdgesChange = useCallback(
+    (changes: EdgeChange[]) =>
+      setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+    []
+  );
+  const onConnect = useCallback(
+    (params: Connection) =>
+      setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+    []
+  );
+
+  return (
+    <div className="size-full">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      />
+    </div>
+  );
 };
