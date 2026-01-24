c0d86e69ffbd7c4fa1f9461b71e928263ec3629f
Ritabrata Ghosh <76sonali40@gmail.com>
2026-01-25 00:29:36 +0530

    toposort done for workflows

--- app/inggest/functions.ts
+++ app/inggest/functions.ts
@@ -1,6 +1,7 @@
 import { NonRetriableError } from "inngest";
 import { inngest } from "./client";
 import { prisma } from "@/lib/db";
+import { topologicalSort } from "./utils";
 
 export const executeWorkflow = inngest.createFunction(
   { id: "execute-workflow" },
@@ -10,7 +11,7 @@ export const executeWorkflow = inngest.createFunction(
     if (!workflowId) {
       throw new NonRetriableError("workflowId is missing");
     }
-    const nodes = await step.run("prepare-workflow", async () => {
+    const sortedNodes = await step.run("prepare-workflow", async () => {
       const workflow = await prisma.workflow.findUniqueOrThrow({
         where: { id: workflowId },
         include: {
@@ -18,9 +19,8 @@ export const executeWorkflow = inngest.createFunction(
           connections: true,
         },
       });
-      return workflow.nodes;
+      return topologicalSort(workflow.nodes, workflow.connections);
     });
-    return { nodes };
+    return { sortedNodes };
   },
 );
-  
